a268357f85fde43f7bef55e90f89916fb190508f
0XSoumya <soumyasahoo.2907@gmail.com>
2026-04-21 01:45:44 +0530

    fixee base url issue

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+const BASE_URL = "https://curalink-83o7.onrender.com"
 
 // 🔹 Create new session
 export async function createSession() {
